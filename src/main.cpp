cf7e2af5cda7c8c0d4db6d538971260f5ecf154b
AjinkyaBhave <ajinkyaybhave@gmail.com>
2018-08-28 09:19:07 +0530

    debugging lane change

--- src/main.cpp
+++ src/main.cpp
@@ -300,15 +300,12 @@ int main() {
 					if((obstacle_s > car_s) && (obstacle_s-car_s < cp_inc)){
 						// Set flag to take safe actions
 						obstacle_close = true;
-						if(lane < 2){
-							lane += 1;
-						}
 						// FSM for changing lanes
-						/*if(lane > 0){
+						if(lane > 0){
 							lane -= 1;
 						}else if (lane < 2){
 							lane +=1;
-						}*/
+						}
 					}
 				}
 			}
