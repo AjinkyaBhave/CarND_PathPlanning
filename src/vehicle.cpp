f1d30d8ce9e0b182ffc222bf5d42244393e9eb51
AjinkyaBhave <ajinkyaybhave@gmail.com>
2018-08-30 13:30:42 +0530

    Adding lane behaviour

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -21,7 +21,7 @@ Vehicle::Vehicle(int lane, double max_ref_vel){
 
 Vehicle::~Vehicle(){}
 
-bool Vehicle::get_vehicle_ahead(vector< vector<double> > sensor_fusion ) {
+bool Vehicle::get_surrounding_vehicles(vector< vector<double> > sensor_fusion ) {
 	// Assume limits for max and min distances in [m] for obstacle cars in all lanes
 	double cur_min_front_s		= 1000;
 	double cur_max_rear_s		= 0;
@@ -45,7 +45,7 @@ bool Vehicle::get_vehicle_ahead(vector< vector<double> > sensor_fusion ) {
 	for (int i = 0; i< sensor_fusion.size(); i++){
 		d = sensor_fusion[i][6];
 		// Check for front and rear vehicle in same lane
-		if (obstacle_d < 4+car.lane*4 && obstacle_d > 4*car.lane){
+		if ((obstacle_d > 4*car.lane) && (obstacle_d < 4+4*car.lane)){
 			
 			obstacle_s = sensor_fusion[i][5];
 			// Check if this is closest front car
@@ -56,8 +56,14 @@ bool Vehicle::get_vehicle_ahead(vector< vector<double> > sensor_fusion ) {
 				car.cur_front_car = true;
 			}else if(){
 				
-			}	
-		}else if(d < 4+car.lane*4 && d > 4*car.lane){
+			}
+		}
+		// Check for front and rear vehicle in left lane
+		else if((obstacle_d > 0) && (obstacle_d < 4*car.lane)){
+			
+		}
+		// Check for front and rear vehicle in right lane
+		else if((obstacle_d > 4*car.lane+4) && (obstacle_d < 4*car.lane+8)){
 			
 		}
 	}	
@@ -75,21 +81,6 @@ bool Vehicle::get_vehicle_ahead(vector< vector<double> > sensor_fusion ) {
 			car.lane +=1;
 		}*/
 	
-	// Returns a true if a vehicle is found ahead of the current vehicle, false otherwise. The passed reference
-    // rVehicle is updated if a vehicle is found.
-    
-    int min_s = this->goal_s;
-    bool found_vehicle = false;
-    Vehicle temp_vehicle;
-    for (map<int, vector<Vehicle>>::iterator it = predictions.begin(); it != predictions.end(); ++it) {
-        temp_vehicle = it->second[0];
-        if (temp_vehicle.lane == this->lane && temp_vehicle.s > this->s && temp_vehicle.s < min_s) {
-            min_s = temp_vehicle.s;
-            rVehicle = temp_vehicle;
-            found_vehicle = true;
-        }
-    }
-    return found_vehicle;
 }
 
 bool Vehicle::get_vehicle_behind(map<int, vector<Vehicle>> predictions, int lane, Vehicle & rVehicle) {
